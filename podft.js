37ed65a86cf829fee4ec772e9129337d501d6d9b
tjomik <tjomik1996@gmail.com>
2019-04-25 21:44:53 +0300

    11

--- podft.js
+++ podft.js
@@ -2,3 +2,15 @@ function autocomplete(){
 a = document.getElementById("ind_99");
 a.click();
 }
+
+function showElements(){
+    var btn = document.getElementById('completeButton');
+    var text = document.createElement('div');
+    text.innerHTML = 'AAAA';
+    document.body.appendChild(text);
+    btn.innerHTML = 'Заполнить';
+    btn.onclick = autocomplete();
+    document.body.appendChild(btn);
+
+}
+document.addEventListener('DOMContentLoaded', showElements);
